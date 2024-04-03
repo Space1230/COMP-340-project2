d0683caaef4f98ac0a7356ddbc4f320f7e904da2
Space1230 <sandylovesapples@yahoo.com>
2024-04-03 19:20:20 -0400

    basic project structure

--- dp.c
+++ dp.c
@@ -1,1 +1,26 @@
 #include "dp.h"
+#include <stdio.h> // for debuging
+
+//function for getting a random number from the list
+int get_next_number() {
+    return 1;
+}
+
+//function that simulates the philosopher operation
+void *philosopher(void *param) {
+    return (void*) 0;
+}
+
+//function for the philosopher to pickup the chopsticks
+void pickup_chopsticks(int number) {
+
+}
+
+//function for the philosopher to return the chopsticks
+void return_chopsticks(int number) {
+
+}
+
+int main() {
+    printf("Hello World\n");
+}
