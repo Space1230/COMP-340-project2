3901bdfcd79d9d1e44e24ef77b30a2ea1fe7efbb
Space1230 <sandylovesapples@yahoo.com>
2024-04-03 20:49:31 -0400

    fixed the parsing

--- dp.c
+++ dp.c
@@ -4,7 +4,11 @@
 
 //function for getting a random number from the list
 int get_next_number() {
-    return 1;
+    if (rand_position == MAX_LENGTH) {
+        fprintf(stderr, "Exhasted random numbers\n");
+        exit(EXIT_FAILURE);
+    }
+    return rand_numbers[rand_position++];
 }
 
 //function that simulates the philosopher operation
@@ -28,11 +32,11 @@ int main(int argc, char* argv[]) {
     // Argument Error Checking
     if (argc < 2) {
         fprintf(stderr, "Please insert your random nums file\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
     else if (argc > 2) {
         fprintf(stderr, "Too many commands, only include your random nums file\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
     // Parsing Random Num File
@@ -40,20 +44,19 @@ int main(int argc, char* argv[]) {
     FILE* random_num_file = fopen(filename, "r");
     if (random_num_file == NULL) {
         fprintf(stderr, "File name doesn't exist, maybe try putting it in quotes\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
     char input;
     rand_position = 0;
     while ((input = fgetc(random_num_file)) != EOF) {
-        if (isblank(input) && rand_position < MAX_LENGTH){
-            rand_numbers[rand_position++] = (char) fgetc(random_num_file);
+        if (!isblank(input) && rand_position < MAX_LENGTH){
+            rand_numbers[rand_position++] = input;
         }
     }
     rand_position = 0; // resetting for future use
 
-    // testing parsing
-    /* for (int i = 0; i < MAX_LENGTH; i++) { */
-    /*     printf("%c ", rand_numbers[i]); */
-    /* } */
-    /* printf("\n"); */
+    for (int i = 0; i < MAX_LENGTH; i++) {
+        printf("%c ", rand_numbers[i]);
+    }
+    printf("\n");
 }
