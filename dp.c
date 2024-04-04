3cd6f8494da1888f553882eb7145b640ac0500ad
Space1230 <sandylovesapples@yahoo.com>
2024-04-03 20:37:48 -0400

    can parse random nums now!!

--- dp.c
+++ dp.c
@@ -1,5 +1,6 @@
 #include "dp.h"
 #include <stdio.h> // for debuging
+#include <ctype.h>
 
 //function for getting a random number from the list
 int get_next_number() {
@@ -21,6 +22,38 @@ void return_chopsticks(int number) {
 
 }
 
-int main() {
-    printf("Hello World\n");
+#define BUFFER_LEN MAX_LENGTH * 2
+
+int main(int argc, char* argv[]) {
+    // Argument Error Checking
+    if (argc < 2) {
+        fprintf(stderr, "Please insert your random nums file\n");
+        return -1;
+    }
+    else if (argc > 2) {
+        fprintf(stderr, "Too many commands, only include your random nums file\n");
+        return -1;
+    }
+
+    // Parsing Random Num File
+    char* filename = argv[1];
+    FILE* random_num_file = fopen(filename, "r");
+    if (random_num_file == NULL) {
+        fprintf(stderr, "File name doesn't exist, maybe try putting it in quotes\n");
+        return -1;
+    }
+    char input;
+    rand_position = 0;
+    while ((input = fgetc(random_num_file)) != EOF) {
+        if (isblank(input) && rand_position < MAX_LENGTH){
+            rand_numbers[rand_position++] = (char) fgetc(random_num_file);
+        }
+    }
+    rand_position = 0; // resetting for future use
+
+    // testing parsing
+    /* for (int i = 0; i < MAX_LENGTH; i++) { */
+    /*     printf("%c ", rand_numbers[i]); */
+    /* } */
+    /* printf("\n"); */
 }
